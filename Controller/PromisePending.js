68d78292497a706ce68ef9b574780a59529caaaa
ARUN-M-S <arunms96official@gmail.com>
2022-08-04 22:31:55 +0530

    "FInished"

---
+++ Controller/PromisePending.js
@@ -0,0 +1,8 @@
+const PromisePendingf=(userId)=>{
+    return new Promise(async(resolve,reject)=>{
+        let result = await //some condiotion or functions for getting the result
+      
+        resolve(result)
+      
+    })
+  }
